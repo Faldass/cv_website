0dce94e230d6797de8be64750d52152ea0f3bbd6
Quentin Terzi <quentin.terzi@outlook.fr>
2023-09-27 12:45:39 +0200

    Ajout fontawesome

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import router from './router'
 import store from './store'
 import 'bootstrap'
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {fab} from '@fortawesome/free-brands-svg-icons'
 
-createApp(App).use(store).use(router).use(bootstrap).mount('#app')
+library.add(fas, fab)
+
+createApp(App).use(store).use(router).use(bootstrap).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
