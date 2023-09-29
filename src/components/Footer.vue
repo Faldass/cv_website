33e0d2a06699afd6aa42f5a27507c82f9ed68aa3
Quentin Terzi <quentin.terzi@outlook.fr>
2023-09-29 15:03:10 +0200

    Amélioration de la responsivité

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,26 @@
 <template>
-    <footer class="container-fluid fixed-bottom border py-3 d-flex justify-content-between">
-        <section class="col-2 border">
-            <p><small>&copy;2023 </small>Quentin Terzi</p>
-        </section>
-        <section class="col-6 border d-flex justify-content-evenly align-items-center">
-            <article>
-                <fa :icon="['fas', 'phone']"></fa>
-                <p>06.43.29.81.73</p>
-            </article>
-            <article>
-                <fa :icon="['fas', 'at']" />
-                <p >quentin.terzi@outlook.fr</p>
-            </article>
-            <article>
-                <fa :icon="['fab', 'linkedin']" class="me-2"/>
-                <fa :icon="['fab', 'github']" />
-            </article>
-        </section>
+    <footer class="container-fluid fixed-bottom border py-3"> <!-- bleu -->
+        <div class="row justify-content-between"> <!-- vert -->
+            <section class="col-2 border align-self-center"> <!-- rouge -->
+                <p><small>&copy;2023 </small>Quentin Terzi</p>
+            </section>
+            <section class="col-6 border"> <!-- Jaune -->
+                <div class="row">
+                    <article class="col border">
+                        <fa :icon="['fas', 'phone']"></fa>
+                        <p>06.43.29.81.73</p>
+                    </article>
+                    <article class="col border">
+                        <fa :icon="['fas', 'at']" />
+                        <p >quentin.terzi@outlook.fr</p>
+                    </article>
+                    <article class="col border align-self-center">
+                        <fa :icon="['fab', 'linkedin']" class="me-2"/>
+                        <fa :icon="['fab', 'github']" />
+                    </article>
+                </div>
+            </section>
+        </div>
     </footer>
 </template>
 
