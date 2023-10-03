45dd7ff4b25b24f7e41d38fe109656bd802f6ae8
Quentin Terzi <quentin.terzi@outlook.fr>
2023-10-03 22:28:57 +0200

    Réorganisation composants et affichage sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,45 @@
 <template>
-  <h1>Salut</h1>
+    <section class="container p-5" v-for="(section, index) in sections" :key="index">
+        <div class="text-start mb-5">
+            <h2 class="fs-1"><fa :icon="section.icon"/> {{ section.title }}</h2>
+        </div>
+        <component :is="section.component"/>
+    </section>
 </template>
 
 <script>
+    import accordionDiplomas from '@/components/Accordion.vue';
 // @ is an alias to /src
-export default {
-  name: 'HomeView',
-  components: {
-  }
-}
+    export default {
+        name: 'HomeView',
+        props: {
+        },
+        components: {
+            accordionDiplomas
+        },
+        data(){
+            return{
+                sections: [
+                    {
+                        title: "Diplômes",
+                        icon: ['fas', 'graduation-cap'],
+                        component: 'accordionDiplomas',
+                    },
+                    {
+                        title: "Compétences",
+                        icon: ['fas', 'list-check'],
+                    },
+                ]
+            }
+        }
+    }
 </script>
+
+<style scoped>
+    section{
+        background-color: #202225;
+        box-shadow: 0 0 15px 5px rgba(1,1,1,.2);
+        color: whitesmoke;
+        margin-bottom: 40px;
+    }
+</style>
